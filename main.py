207cf051d44c0f25b4e95a4d223da4e7035c6582
Mario-Gijon <gijonlietormario@gmail.com>
2024-07-15 11:30:50 +0200

    first commit

--- main.py
+++ main.py
@@ -6,20 +6,13 @@ import requests
 import pandas as pd
 import ast
 
-# Configuración básica de logging
 logging.basicConfig(level=logging.INFO) 
+app = FastAPI()
+
 class MovieOnDB(BaseModel):
   idTmdb: int
   rate: int
 
-
-
-app = FastAPI()
-listOfAllMovies = []
-#moviesRated: List[MovieOnDB] = [MovieOnDB(idTmdb=502356, rate=4), MovieOnDB(idTmdb=1022789, rate=5)]
-dfMovies = None
-page = 1
-# Lista de géneros con sus id y nombres
 genres_list = [
   {"id": 28, "name": "Action"},
   {"id": 12, "name": "Adventure"},
@@ -42,9 +35,9 @@ genres_list = [
   {"id": 37, "name": "Western"}
 ]
 genres_dict = {genre['id']: genre['name'] for genre in genres_list}
-
-
-
+listOfAllMovies = []
+dfMovies = None
+page = 1
 
 
 def getMoviesFromTmdbApi():
@@ -56,32 +49,33 @@ def getMoviesFromTmdbApi():
     "with_genres": "10751",
     "language": "en-US",
   }
-  response = requests.get(
-    f"https://api.themoviedb.org/3/discover/movie?with_genres=10751&page={page}&sort_by=popularity.desc",
-    headers=headers
-  )
+  
+  response = requests.get(f"https://api.themoviedb.org/3/discover/movie?with_genres=10751&page={page}&sort_by=popularity.desc",headers=headers)
   if response.status_code == 200:
+    
     if page < 90:
       listOfAllMovies.extend(response.json()['results'])
       print(f"Page {page} success")
       page += 1
       return getMoviesFromTmdbApi()
+    
     else:
       page = 1
       getCsv()
       return True
+    
   else:
     print("Rejected connection")
     return False
 
+
 def getCsv():
   dataset = []
   global listOfAllMovies
+  
   for movie in listOfAllMovies:
     #keywords = next((item['keywords'] for item in listOfKeywords if item['id'] == movie_id), [])
     #reviews = next((item['results'] for item in listOfReviews if item['id'] == movie_id), [])
-    
-    
     movie_data = {
       'id': movie['id'],
       'title': movie['title'],
@@ -94,42 +88,49 @@ def getCsv():
       #'keywords': [keyword['name'] for keyword in keywords],
       #'reviews': [{'author': review['author'], 'content': review['content'], 'rating': review['author_details'].get('rating')} for review in reviews]
     }
+    
     dataset.append(movie_data)
+    
   df = pd.DataFrame(dataset)
   df.to_csv('movies_dataset.csv', index=False)
 
+
 def getUserProfile(moviesRated):
   global dfMovies
-  # Leer el archivo CSV
+  rows = []
+
   dfMovies = pd.read_csv('movies_dataset.csv')
   dfMovies['genre_id'] = dfMovies['genre_id'].apply(ast.literal_eval)
-  # Extraer los IDs y las puntuaciones de las películas de la lista de objetos MovieOnDB
+
   movie_ids = [movie.idTmdb for movie in moviesRated]
   ratings = {movie.idTmdb: movie.rate for movie in moviesRated}
-  # Filtrar el DataFrame para obtener solo las películas en movie_ids
+
   filteredMovies = dfMovies[dfMovies['id'].isin(movie_ids)]
-  # Crear una lista de filas para cada película y su rating
-  rows = []
+
   for _, row in filteredMovies.iterrows():
-      for genre_id in row['genre_id']:
-          rows.append({'genre_id': genre_id, 'rating': ratings[row['id']]})
-  # Convertir las filas a un DataFrame
+    for genre_id in row['genre_id']:
+      rows.append({'genre_id': genre_id, 'rating': ratings[row['id']]})
+
   dfRatings = pd.DataFrame(rows)
-  # Agrupar por genre_id y sumar las puntuaciones
   gender_counts = dfRatings.groupby('genre_id')['rating'].sum().to_dict()
-  # Imprimir el diccionario de puntuaciones por género
+
   return gender_counts
+
 
 def getScores(userProfile):
   global dfMovies
   res = {}
+  
   for index, row in dfMovies.iterrows():
-      movie_id = row['id']
-      score = 0
-      for genre_id in row['genre_id']:
-          if genre_id in userProfile:
-              score += userProfile[genre_id]
-      res[movie_id] = score
+    movie_id = row['id']
+    score = 0
+    
+    for genre_id in row['genre_id']:
+      if genre_id in userProfile:
+        score += userProfile[genre_id]
+        
+    res[movie_id] = score
+    
   return res
 
 
@@ -139,21 +140,20 @@ def index(moviesRated: List[MovieOnDB]):
   
   userProfile = getUserProfile(moviesRated)
   scores = getScores(userProfile)
-  # Imprimir los resultados para cada película
+  
   sorted_movies = sorted(scores.items(), key=lambda x: x[1], reverse=True)
   filtered_movies = [(movie_id, score) for movie_id, score in sorted_movies if not any(movie_id == rated.idTmdb for rated in moviesRated)]
 
   for id, score in filtered_movies:
-    print(str(id) + " - " + str(score))
+    #print(str(id) + " - " + str(score))
     recommend = {
       "id": str(id),
-      "txt": f"Este es el texto de la película con id {id} y con un score de {score}."
-    }
+      "txt": f"Este es el texto de la película con id {id}, con un score de {score}."
+    }  
     recommends.append(recommend)
   
-
-  
   return {"Recommends": recommends}
+
 
 @app.get("/testAllMovies")
 def allMovies():
@@ -165,16 +165,18 @@ def allMovies():
 
 if getMoviesFromTmdbApi():
   print("Conection to TMDB -> success")
+  
   """
+  Esto es para poder hacer las comprobaciones usando directamente la terminal y no el servidor
   recommends = []
+  moviesRated: List[MovieOnDB] = [MovieOnDB(idTmdb=502356, rate=4), MovieOnDB(idTmdb=1022789, rate=5)]
   
   userProfile = getUserProfile(moviesRated)
   scores = getScores(userProfile)
-  # Imprimir los resultados para cada película
+  
   sorted_movies = sorted(scores.items(), key=lambda x: x[1], reverse=True)
   for id, score in sorted_movies:
     print(str(id) + " - " + str(score))
-    #logging.info(f"Received movie with idTmdb: {movie.idTmdb} and rate: {movie.rate}")
     recommend = {
       "id": str(id),
       "txt": f"Este es el texto de la película con id {id} y con un score de {score}."
@@ -183,5 +185,6 @@ if getMoviesFromTmdbApi():
 
   print(recommends)
   """
+  
 else:
   print("Error getting films from TMDB API")
